44cd61609c9a19bf6e98cf4c48bc6c7e0ff8ae5f
bruno-rocha-movile <bruno.rocha@movile.com>
2016-08-15 17:13:54 -0300

    Carthage support

---
+++ ErrorKingCarthageResources/ErrorKing.h
@@ -0,0 +1,19 @@
+//
+//  ErrorKing.h
+//  ErrorKing
+//
+//  Created by Bruno Rocha on 8/15/16.
+//  Copyright © 2016 Movile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ErrorKing.
+FOUNDATION_EXPORT double ErrorKingVersionNumber;
+
+//! Project version string for ErrorKing.
+FOUNDATION_EXPORT const unsigned char ErrorKingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ErrorKing/PublicHeader.h>
+
+
